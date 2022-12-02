190f9da654dc1cdd27a2b8406c58335e8a820b59
Mehedi Hasan <mehedihasan2882@icloud.com>
2022-12-02 19:57:07 +0600

    Updated the JS file
    Added random colour to the font of the button. It will be more appealing than before. 
    All the functions of code are described by using comments.

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-
-// Change if user hover over the button
+// Change if user click over the button
 let changes=document.querySelector("button")
 changes.addEventListener("click", function(){
+// Producing random color palate:
    let r= Math.floor(Math.random()*255)
    let g= Math.floor(Math.random()*255)
    let b= Math.floor(Math.random()*255)
    let a= Math.floor(Math.random()*255)
    let c= Math.floor(Math.random()*255)
+   let d= Math.floor(Math.random()*255)
+
+// Change the background color randomly
    let newColor= `rgb(${r},${g},${b})`
    document.body.style.backgroundColor=newColor
 // Change the color of the heading randomly
-   let heading_color=`rgb(${g},${r},${a})`
+   let heading_color=`rgb(${d},${r},${a})`
    document.querySelector("h1").style.color=heading_color
 // Change the color of the sub-heading randomly
    let subHeadColor=`rgb(${b}, ${g}, ${c})`
@@ -18,7 +21,9 @@ changes.addEventListener("click", function(){
 // Change the button color randomly
    let btnColor=`rgba(${a}, ${c}, ${g})`
    document.querySelector("button").style.backgroundColor=btnColor
+// Change the font color of the button randomly
+   let fontBtn=`rgb(${d},${a},${r})`
+   document.querySelector("button").style.color=fontBtn
 })
 
-// Change if user click on the button
 
